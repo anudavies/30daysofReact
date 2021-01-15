9a97239fdb52da06fd898a90319aa1b18d99dd62
anudavies <anudavies@gmail.com>
2021-01-15 14:35:01 +0100

    major compnents added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from './components/header/Header'
+import Main from './components/main/Main'
+import Footer from './components/footer/Footer'
+import { countriesData } from './data/countries'
+import asabenehImage from './assets/images/asabeneh.jpg'
+import { showDate } from './utils/display-date-and-time'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    loggedIn: false,
+    techs: ['HTML', 'CSS', 'JS'],
+    message: 'Click show time or Greet people to change me',
+    country: countriesData[1],
+  }
+  handleLogin = () => {
+    this.setState({
+      loggedIn: !this.state.loggedIn,
+    })
+  }
+  handleTime = () => {
+    let message = showDate(new Date())
+    this.setState({ message })
+  }
+  greetPeople = () => {
+    let message = 'Welcome to 30 Days Of React Challenge, 2020'
+    this.setState({ message })
+  }
+
+  render() {
+    const data = {
+      welcome: '30 Days Of React',
+      title: 'Getting Started React',
+      subtitle: 'JavaScript Library',
+      author: {
+        firstName: 'Asabeneh',
+        lastName: 'Yetayeh',
+      },
+      date: new Date(),
+    }
+    const techs = ['HTML', 'CSS', 'JavaScript']
+    const user = { ...data.author, image: asabenehImage }
+
+    return (
+      <div className='app'>
+        {this.state.backgroundColor}
+        <Header data={data} />
+        <Main
+          techs={techs}
+          handleTime={this.handleTime}
+          greetPeople={this.greetPeople}
+          loggedIn={this.state.loggedIn}
+          handleLogin={this.handleLogin}
+          message={this.state.message}
+          country={this.state.country}
+          user={user}
+        />
+
+        <Footer date={new Date()} />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
