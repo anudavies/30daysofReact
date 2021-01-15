c2ce5362ae0b05b84950ff5c9a4ef5abd83b08dd
anudavies <anudavies@gmail.com>
2021-01-15 14:59:49 +0100

    images added

--- src/App.js
+++ src/App.js
@@ -33,8 +33,8 @@ class App extends React.Component {
       title: 'Getting Started React',
       subtitle: 'JavaScript Library',
       author: {
-        firstName: 'Asabeneh',
-        lastName: 'Yetayeh',
+        firstName: 'David',
+        lastName: 'Egbebunmi',
       },
       date: new Date(),
     }
